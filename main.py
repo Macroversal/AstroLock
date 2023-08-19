7e2957abf3c5569da4fedbef55119447a99d6863
Michael Mahoe <88037418+Macroversal@users.noreply.github.com>
2023-08-19 01:32:12 -0500

    Project: Data Encryption Tool    - Description: Developed a data encryption tool using Python and cryptography libraries. Implemented symmetric and asymmetric encryption algorithms to protect sensitive data. Provided an easy-to-use command-line interface for encryption and decryption operations.

---
+++ main.py
@@ -0,0 +1,87 @@
+import logging
+from cryptography.fernet import Fernet
+import getpass
+import sys
+
+class EncryptionApp:
+    def __init__(self):
+        self.key = None
+
+    def generate_key(self):
+        return Fernet.generate_key()
+
+    def encrypt_message(self, message, key):
+        cipher_suite = Fernet(key)
+        encrypted_message = cipher_suite.encrypt(message.encode())
+        return encrypted_message
+
+    def run(self):
+        logging.basicConfig(filename='encryption_app.log', level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        self.key = self.generate_key()
+        logging.info("Encryption Key generated")
+
+        print("Encryption Key:", self.key.decode())
+        logging.info("Encryption Key displayed")
+
+        message = input("Enter the message to encrypt: ")
+
+        try:
+            encrypted_message = self.encrypt_message(message, self.key)
+            print("Message Encrypted Successfully.")
+            print("Encrypted Message:", encrypted_message.decode())
+            logging.info("Message Encrypted Successfully")
+        except Exception as e:
+            print("An error occurred during encryption:", e)
+            logging.error("An error occurred during encryption: %s", e)
+
+class DecryptionApp:
+    def decrypt_message(self, encrypted_message, decryption_key):
+        cipher_suite = Fernet(decryption_key)
+        decrypted_message = cipher_suite.decrypt(encrypted_message)
+        return decrypted_message.decode()
+
+    def validate_hex_key(self, key_input):
+        return all(c in '0123456789abcdefABCDEF' for c in key_input)
+
+    def validate_hex_message(self, message_input):
+        return all(c in '0123456789abcdefABCDEF' for c in message_input)
+
+    def run(self):
+        logging.basicConfig(filename='decryption_app.log', level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+
+        try:
+            decryption_key = getpass.getpass("Enter the decryption key (hex format): ").strip()
+            if not self.validate_hex_key(decryption_key):
+                print("Invalid decryption key format. Please enter the key in hex format.")
+                logging.error("Invalid decryption key format.")
+                return
+            logging.info("Decryption Key entered securely")
+
+            encrypted_message_input = input("Enter the encrypted message (hex format): ").strip()
+            if not self.validate_hex_message(encrypted_message_input):
+                print("Invalid encrypted message format. Please enter the message in hex format.")
+                logging.error("Invalid encrypted message format.")
+                return
+
+            encrypted_message = bytes.fromhex(encrypted_message_input)
+            decrypted_message = self.decrypt_message(encrypted_message, decryption_key.encode())
+            print("Decrypted Message:", decrypted_message)
+            logging.info("Message Decrypted Successfully")
+        except KeyboardInterrupt:
+            print("\nDecryption process interrupted.")
+            logging.info("Decryption process interrupted.")
+        except cryptography.fernet.InvalidToken:
+            print("Decryption failed. The provided key might be incorrect or the message has been tampered with.")
+            logging.error("Decryption failed")
+        except Exception as e:
+            print("An error occurred during decryption:", e)
+            logging.error("An error occurred during decryption: %s", e)
+
+if __name__ == "__main__":
+    encryption_app = EncryptionApp()
+    encryption_app.run()
+
+    decryption_app = DecryptionApp()
+    decryption_app.run()
