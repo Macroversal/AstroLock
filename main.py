1c2010e95e24554ecbc295f3c145d0f622a42b8d
Michael Mahoe <88037418+Macroversal@users.noreply.github.com>
2023-08-22 06:33:14 -0500

    8/22/2023 - Encapsulation and Modularity: Encapsulated the authentication logic into its own method in the DecryptionApp class.

--- main.py
+++ main.py
@@ -38,70 +38,63 @@ class EncryptionApp:
             logging.error("An error occurred during encryption: %s", e)
 
 class DecryptionApp:
-    def decrypt_message(self, encrypted_message, decryption_key):
-        cipher_suite = Fernet(decryption_key)
-        decrypted_message = cipher_suite.decrypt(encrypted_message)
-        return decrypted_message.decode()
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
 
     def authenticate(self):
         username = input("Enter your username: ")
         password = getpass.getpass("Enter your password: ")
-        # Here you can perform authentication based on username and password
-        # For demonstration purposes, let's assume username is "admin" and password is "password"
-        if username == "admin" and password == "password":
-            return True
-        else:
-            return False
+        return username == "admin" and password == "password"
+
+    def validate_hex_key(self, key_input):
+        return all(c in '0123456789abcdefABCDEF' for c in key_input)
+
+    def validate_hex_message(self, message_input):
+        return all(c in '0123456789abcdefABCDEF' for c in message_input)
+
+    def decrypt_message(self, encrypted_message, decryption_key):
+        cipher_suite = Fernet(decryption_key)
+        decrypted_message = cipher_suite.decrypt(encrypted_message)
+        return decrypted_message.decode()
 
     def run(self, decryption_key, encrypted_message_input):
         if not self.authenticate():
             print("Authentication failed.")
             return
 
-        def validate_hex_key(self, key_input):
-            return all(c in '0123456789abcdefABCDEF' for c in key_input)
-
-        def validate_hex_message(self, message_input):
-            return all(c in '0123456789abcdefABCDEF' for c in message_input)
-
-        def run(self, decryption_key, encrypted_message_input):
-            if not self.authenticate():
-                print("Authentication failed.")
+        try:
+            decryption_key = decryption_key.strip()
+            if not self.validate_hex_key(decryption_key):
+                print("Invalid decryption key format. Please enter the key in hex format.")
                 return
 
-            try:
-                decryption_key = decryption_key.strip()
-                if not self.validate_hex_key(decryption_key):
-                    print("Invalid decryption key format. Please enter the key in hex format.")
-                    return
+            self.logger.info("Decryption Key entered securely")
 
-                logging.info("Decryption Key entered securely")
+            encrypted_message_input = encrypted_message_input.strip()
+            if not self.validate_hex_message(encrypted_message_input):
+                print("Invalid encrypted message format. Please enter the message in hex format.")
+                return
 
-                encrypted_message_input = encrypted_message_input.strip()
-                if not self.validate_hex_message(encrypted_message_input):
-                    print("Invalid encrypted message format. Please enter the message in hex format.")
-                    return
-
-                encrypted_message = bytes.fromhex(encrypted_message_input)
-                decrypted_message = self.decrypt_message(encrypted_message, decryption_key.encode())
-                print("Decrypted Message:", decrypted_message)
-                logging.info("Message Decrypted Successfully")
-            except KeyboardInterrupt:
-                print("\nDecryption process interrupted.")
-                logging.info("Decryption process interrupted.")
-            except cryptography.fernet.InvalidToken:
-                print("Decryption failed. The provided key might be incorrect or the message has been tampered with.")
-                print("Make sure the decryption key and encrypted message are correct.")
-            except Exception as e:
-                print("An error occurred during decryption:", e)
-                print("Please check if the decryption key and encrypted message are correct.")
-
-    if __name__ == "__main__":
+            encrypted_message = bytes.fromhex(encrypted_message_input)
+            decrypted_message = self.decrypt_message(encrypted_message, decryption_key.encode())
+            print("Decrypted Message:", decrypted_message)
+            self.logger.info("Message Decrypted Successfully")
+        except KeyboardInterrupt:
+            print("\nDecryption process interrupted.")
+            self.logger.info("Decryption process interrupted.")
+        except cryptography.fernet.InvalidToken:
+            print("Decryption failed. The provided key might be incorrect or the message has been tampered with.")
+            print("Make sure the decryption key and encrypted message are correct.")
+        except Exception as e:
+            print("An error occurred during decryption:", e)
+            print("Please check if the decryption key and encrypted message are correct.")
+if __name__ == "__main__":
         parser = argparse.ArgumentParser(description="Python Encryption/Decryption Tool")
         parser.add_argument("--encrypt", help="Encrypt a message")
         parser.add_argument("--decrypt",
                             help="Decrypt a message (requires decryption key and encrypted message arguments)")
-        parser.add_argument("--decryption_key", help="Decryption key (hex format)", required="--decrypt" in sys.argv)
+        parser.add_argument("--decryption_key", help="Decryption key (hex format)",
+                            required="--decrypt" in sys.argv)
         parser.add_argument("--encrypted_message", help="Encrypted message (hex format)",
                             required="--decrypt" in sys.argv)
 
